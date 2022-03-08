44284a60155ca4ef014053653444daf598c7e2fd
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-03-08 20:52:00 +0100

    [BUGFIX] Avoid undefined array key warnings in PHP8
    PHP Warning: Undefined array key "tslib/class.tslib_fe.php" in /var/www/html/vendor/t3/min/ext_localconf.php line 18
    
    Without TypoScript embedding:
    PHP Warning: Undefined array key "tx_min." in /var/www/html/vendor/t3/min/Classes/Tinysource.php line 49
    PHP Warning: Undefined array key "enable" in /var/www/html/vendor/t3/min/Classes/Tinysource.php line 51
    
    After TypoScript embedding:
    PHP Warning: Undefined array key "head." in /var/www/html/vendor/t3/min/Classes/Tinysource.php line 194
    PHP Warning: Undefined array key "head." in /var/www/html/vendor/t3/min/Classes/Tinysource.php line 109
    PHP Warning: Undefined array key "head." in /var/www/html/vendor/t3/min/Classes/Tinysource.php line 124

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,9 +15,7 @@ $GLOBALS['TYPO3_CONF_VARS']['FE']['cssCompressHandler'] = T3\Min\Minifier::class
 $GLOBALS['TYPO3_CONF_VARS']['FE']['jsCompressHandler'] = T3\Min\Minifier::class .  '->minifyJavaScript';
 
 // Register tiny source
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'] = [];
-}
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'] ??= [];
 array_unshift(
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'],
     'T3\Min\Tinysource->tinysource'
