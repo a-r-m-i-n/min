73f960b9dd332c72770d474c5d1e880deeefb027
Kevin Meier <kontakt@network-media.de>
2023-02-14 17:21:32 +0100

    Replace outdated constant TYPO3_branch with Typo3Version class

--- Classes/Compatibility.php
+++ Classes/Compatibility.php
@@ -1,19 +1,21 @@
 <?php declare(strict_types=1);
 namespace T3\Min;
 
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 class Compatibility
 {
-    /**
-     * Checks if current TYPO3 version is 10.0.0 or greater (by default)
-     *
-     * @param string $version e.g. 10.0.0
-     * @return bool
-     */
-    public static function isTypo3Version($version = '10.0.0') : bool
-    {
-        return VersionNumberUtility::convertVersionNumberToInteger(TYPO3_branch) >=
-            VersionNumberUtility::convertVersionNumberToInteger($version);
-    }
+	/**
+	 * Checks if current TYPO3 version is 10.0.0 or greater (by default)
+	 *
+	 * @param string $version e.g. 10.0.0
+	 * @return bool
+	 */
+	public static function isTypo3Version($version = '10') : bool
+	{
+		$typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
+		return $typo3Version->getMajorVersion() >=
+		       explode('.', $version)[0];
+	}
 }
